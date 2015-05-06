3c17fc89891d6a4fc1104c7542fecc3ebb470832
r3h6 <r3h6@outlook.com>
2015-05-06 23:25:54 +0200

    editable

--- Resources/Public/JavaScript/jquery.editable.js
+++ Resources/Public/JavaScript/jquery.editable.js
@@ -1,4 +1,5 @@
 (function ($){
+
 	/**
 	 * @see https://developer.mozilla.org/en/docs/Web/API/HTMLTextAreaElement#Autogrowing_textarea_example
 	 */
@@ -8,52 +9,152 @@
 		}
 	}
 
-	$.fn.editable = function (){
-		return $(this).on('click', '.editable', function (event){
-			var $el = $(this);
-			if ($el.hasClass('edit')) return null;
-			$el.addClass('edit');
+	var Editable = function (el, options){
+		this.el = el;
+		this.$el = $(el);
+		this.options = $.extend(true, {}, $.fn.ajaxForm.defaultOptions, options);
 
-			var content = $el.html();
+		this.$selectedCell = null;
+		this.disableArrows = false;
 
-			var $editor = $('<textarea class="editable-editor" />')
-				.val(content)
-				.css({width: '100%', height: $el.innerHeight() - 16})
-				.on('blur', function (event){
-					var value = $(this).val();
-					$el.removeClass('edit').html(value);
-					if (value != content){
-						$el.trigger('change.editable', [value]);
-					}
-				})
-				.on('focus keyup', function (event){
-					autoGrow(this);
-				})
-				.on('keyup', function (event){
-					var index = $el.parent().children().index($el);
-					switch(event.which){
-						case 37: // left
-							$el.prev().trigger('click');
-							break;
+		// var self = this;
+		this.$el.on('dblclick', '.editable', $.proxy(this.dblclickHandler, this));
+		this.$el.on('click', '.editable', $.proxy(this.clickHandler, this));
+		this.$el.on('keydown', '.editable', $.proxy(this.keypressHandler, this));
+	}
 
-						case 38: // up
-							$el.closest('tr').prev().children(':eq(' + index + ')').trigger('click');
-							break;
+	Editable.VERSION  = '1.0.0';
+	Editable.KEY_LEFT = 37;
+	Editable.KEY_UP = 38;
+	Editable.KEY_RIGHT = 39;
+	Editable.KEY_DOWN = 40;
+	Editable.KEY_TAB = 9;
+	Editable.KEY_ENTER = 13;
 
-						case 39: // right
-							$el.next().trigger('click');
-							break;
 
-						case 40: // down
-							$el.closest('tr').next().children(':eq(' + index + ')').trigger('click');
-							break;
-					}
-				});
+	Editable.prototype.moveLeft = function (){
+		return this.editCell(this.$selectedCell.prev('.editable'));
+	}
 
-			$el.html($editor);
-			$editor.focus();
+	Editable.prototype.moveRight = function (){
+		return this.editCell(this.$selectedCell.next('.editable'));
+	}
 
-			$el.trigger('init.editable', [$editor]);
+	Editable.prototype.moveUp = function (){
+		var index = this.$selectedCell.parent().children().index(this.$selectedCell);
+		return this.editCell(this.$selectedCell.closest('tr').prev().children(':eq(' + index + ')'));
+	}
+
+	Editable.prototype.moveDown = function (){
+		var index = this.$selectedCell.parent().children().index(this.$selectedCell);
+		return this.editCell(this.$selectedCell.closest('tr').next().children(':eq(' + index + ')'));
+	}
+
+	Editable.prototype.next = function (){
+		if (this.moveRight()){
+			return true;
+		}
+		return this.editCell(this.$selectedCell.closest('tr').next().children().filter('.editable').first());
+	}
+
+	Editable.prototype.editCell = function ($cell){
+		if (!$cell.length || !$cell.is('.editable') || $cell.hasClass('edit')) return false;
+
+		this.$selectedCell = $cell;
+		var self = this;
+
+
+		$cell.addClass('edit');
+
+		var content = $cell.html();
+
+		var $editor = $('<textarea class="editable-editor" />')
+			.val(content)
+			.css({width: '100%', height: $cell.innerHeight() - 16})
+			.on('blur', function (event){
+				self.disableArrows = false;
+				var value = $(this).val();
+				$cell.removeClass('edit').html(value);
+				if (value != content){
+					$cell.trigger('change.editable', [value]);
+				}
+			})
+			.on('focus keyup', function (event){
+				autoGrow(this);
+			});
+
+		$cell.html($editor);
+		$editor.focus();
+
+		this.$selectedCell.trigger('init.editable', [$editor]);
+		return true;
+	}
+
+	Editable.prototype.clickHandler = function (event){
+		this.editCell($(event.target));
+	}
+
+	Editable.prototype.dblclickHandler = function (event){
+		this.disableArrows = true;
+		this.editCell($(event.target));
+	}
+
+	Editable.prototype.keypressHandler = function (event){
+
+		switch (event.which){
+			case Editable.KEY_ENTER:
+				event.preventDefault();
+				this.moveDown();
+				break;
+			case Editable.KEY_TAB:
+				event.preventDefault();
+				this.next();
+				break;
+			case Editable.KEY_DOWN:
+				if (!this.disableArrows){
+					this.moveDown();
+				}
+				break;
+			case Editable.KEY_UP:
+				if (!this.disableArrows){
+					this.moveUp();
+				}
+				break;
+			case Editable.KEY_RIGHT:
+				if (!this.disableArrows){
+					this.moveRight();
+				}
+				break;
+			case Editable.KEY_LEFT:
+				if (!this.disableArrows){
+					this.moveLeft();
+				}
+				break;
+		}
+	}
+
+	var Plugin = function (options) {
+		return this.each(function () {
+			var $this = $(this);
+
+			var data  = $this.data('editable');
+
+			if (!data) {
+				$this.data('editable', (data = new Editable(this, options)));
+			}
+			if (typeof options == 'string'){
+				data[options]();
+			}
+
 		});
+	}
+	Plugin.defaultOptions = {
 	};
+
+	$.fn.editable             = Plugin
+	$.fn.editable.Constructor = Editable
+
+	$(document).ready(function() {
+		$('form[data-editable]').editable();
+	});
 }(jQuery));
